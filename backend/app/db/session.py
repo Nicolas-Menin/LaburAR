ef9cd99d9a43d029962f0527777c238ed65c6e46
Nicolas-Menin <meninjuanmanuel542@gmail.com>
2026-07-21 17:48:01 -0300

    feat: agregado de fabrica de sesioness para la base de datos

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from backend.app.core.config import DB_URL
 
 
@@ -6,3 +7,9 @@ from backend.app.core.config import DB_URL
 # CREACION DEL MOTOR DE LA BASE DE DATOS
 engine = create_engine(DB_URL,echo=True,pool_pre_ping=True)
 
+# FABRICA DE SESIONES PARA INTERACTUAR CON LA BASE DE DATIS
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=True
+)
